246d5dfe9395907d3d07052e821c940a8481b16d
alamridoy <alamridoy7@gmail.com>
2024-09-05 23:39:12 +0530

    user

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 // CORS middleware configuration
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","http://localhost:3001","http://localhost:3002",],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],

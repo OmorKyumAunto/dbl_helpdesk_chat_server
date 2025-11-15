6a5b9c261febb64cec59126b44e7701fd47be405
alamridoy <ridoy@dbl-group.com>
2025-11-15 16:47:17 +0600

    bug fixing

--- api/v1/queries/raise-ticket.js
+++ api/v1/queries/raise-ticket.js
@@ -1129,14 +1129,60 @@ let getSuperAdminTicketReport = (
 };
 
 
+// let ticketReportList = (
+//   limit,offset,key, start_date, end_date, category, priority, unit, status, user_id, overdue
+// ) => {
+//   let baseQuery = `
+//     SELECT ticket_table_id, ticket_id, ticket_status, subject, priority, ticket_category_title, asset_serial_number,
+//       ticket_created_employee_name, ticket_created_employee_id,
+//       ticket_solved_employee_name, ticket_solved_employee_id, asset_unit_title, ticket_updated_at, asset_unit_id,
+//       ticket_created_at, is_overdue, seating_unit_name, complex_name, seating_location_name
+//     FROM super_admin_ticket_view
+//   `;
+
+//   let conditions = [];
+
+//   if (unit) conditions.push(`asset_unit_id = '${unit}'`);
+//   if (user_id) conditions.push(`ticket_solved_employee_user_id = '${user_id}'`);
+//   if (status) conditions.push(`ticket_status = '${status}'`);
+//   if (priority) conditions.push(`priority = '${priority}'`);
+//   if (category) conditions.push(`ticket_category_id = '${category}'`);
+//   if (overdue) conditions.push(`is_overdue = '${overdue}'`);
+//   if (start_date && end_date) conditions.push(`ticket_created_at BETWEEN '${start_date}' AND '${end_date}'`);
+//   if (key) {
+//     conditions.push(`(
+//       subject LIKE '%${key}%' 
+//       OR ticket_id LIKE '%${key}%' 
+//       OR ticket_solved_employee_name LIKE '%${key}%' 
+//       OR ticket_created_employee_name LIKE '%${key}%' 
+//       OR ticket_solved_employee_id LIKE '%${key}%' 
+//       OR ticket_created_employee_id LIKE '%${key}%'
+//     )`);
+//   }
+
+//   if (conditions.length > 0) {
+//     baseQuery += " WHERE " + conditions.join(" AND ");
+//   }
+
+//   // Pagination: LIMIT / OFFSET should always be added last
+//   if (limit) {
+//     baseQuery += ` LIMIT ${limit}`;
+//     if (offset) {
+//       baseQuery += ` OFFSET ${offset}`;
+//     }
+//   }
+
+//   return baseQuery;
+// };
 let ticketReportList = (
-  offset, limit, key, start_date, end_date, category, priority, unit, status, user_id, overdue
+  limit = 50, offset = 0, key, start_date, end_date, category, priority, unit, status, user_id, overdue
 ) => {
   let baseQuery = `
     SELECT ticket_table_id, ticket_id, ticket_status, subject, priority, ticket_category_title, asset_serial_number,
-      ticket_created_employee_name, ticket_created_employee_id,
-      ticket_solved_employee_name, ticket_solved_employee_id, asset_unit_title, ticket_updated_at, asset_unit_id,
-      ticket_created_at, is_overdue, seating_unit_name, complex_name, seating_location_name
+           ticket_created_employee_name, ticket_created_employee_id,
+           ticket_solved_employee_name, ticket_solved_employee_id, asset_unit_title,
+           ticket_updated_at, asset_unit_id, ticket_created_at, is_overdue, seating_unit_name,
+           complex_name, seating_location_name
     FROM super_admin_ticket_view
   `;
 
@@ -1164,17 +1210,11 @@ let ticketReportList = (
     baseQuery += " WHERE " + conditions.join(" AND ");
   }
 
-  // Pagination: LIMIT / OFFSET should always be added last
-  if (limit) {
-    baseQuery += ` LIMIT ${limit}`;
-    if (offset) {
-      baseQuery += ` OFFSET ${offset}`;
-    }
-  }
+  // Always add pagination
+  baseQuery += ` LIMIT ${limit} OFFSET ${offset}`;
 
   return baseQuery;
 };
-
 let ticketReport = (
  key,start_date,end_date,category,priority,unit,status,user_id,overdue
 ) => {
